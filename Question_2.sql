85540317117b0609d7b54e6f45d8199ce5cd9782
kevi-3 <113993103+kevi-3@users.noreply.github.com>
2023-10-30 22:56:19 +0530

    Update Question_2.sql

--- Question_2.sql
+++ Question_2.sql
@@ -13,15 +13,15 @@ SELECT
         WHEN E.event_end_time > B.break_end_time THEN B.break_end_time
         ELSE E.event_end_time
     END AS event_end_time
-FROM Events AS E
-JOIN Breaks AS B 
+FROM Event AS E
+JOIN Break AS B 
 	ON E.device_id = B.device_id
     AND E.shift_id = B.shift_id
     AND E.event_start_time < B.break_end_time
     AND E.event_end_time > B.break_start_time;
 
--- Insert the split events into the original events table
-INSERT INTO Events (device_id, shift_id, event_id, event_start_time, event_end_time)
+-- Insert the split events into the original events table with chnages
+INSERT INTO Event (device_id, shift_id, event_id, event_start_time, event_end_time)
 SELECT
     device_id,
     shift_id,
